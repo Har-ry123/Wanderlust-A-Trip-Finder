08deda17b420585a34cf583c09a5d56b62dbdbe1
Hariom Jha <2205037@kiit.ac.in>
2025-08-22 01:08:53 +0530

    first commit

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.use((err, req, res, next) => {
    res.status(statusCode).render("error.ejs", {message});
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
